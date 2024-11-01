968115c0f92118e42e4fdc2d80b1961546bcf3bb
Ryan Toh <ryantoh99@gmail.com>
2024-11-02 01:07:35 +0800

    fix: sanitised class name

--- data.py
+++ data.py
@@ -4,10 +4,21 @@ Provides functions to load local data
 from typing import Dict, Any, List
 
 import json
+import urllib.parse
 from flask import g
 from pathlib import Path
 
 CLASSES_JSON = Path("data/classes.json")
+
+def urlsafe_classname(classname: str) -> str:
+    # Only accept alphanumeric characters, brackets and replace spaces with underscores
+    result = ""
+    for c in classname.lower():
+        if c == '(' or c == ')' or c.isalpha():
+            result += c
+        elif c == ' ':
+            result += '_'
+    return result
 
 def get_classes() -> List[Dict[str, Any]]:
     """ Returns player classes """
@@ -17,5 +28,5 @@ def get_classes() -> List[Dict[str, Any]]:
         class_ls = json.loads(CLASSES_JSON.read_text())
         g.data_classes = {}
         for playerclass in class_ls:
-            g.data_classes[playerclass["name"].lower()] = playerclass
+            g.data_classes[urlsafe_classname(playerclass["name"])] = playerclass
     return g.data_classes
