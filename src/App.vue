774ff4496c3fd2a8c43f4005a4f90a2ff6686fb6
Ryan Toh <ryantoh99@gmail.com>
2024-11-03 22:31:52 +0800

    (!) implement loading of skills from local JSON file
    - JSON description is converted to HTML and sanitised, preventing XSS from imported JSON files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SkillSearch from "@/components/SkillSearch.vue";
+
+</script>
+
+<template>
+    <header>
+
+    </header>
+
+    <main>
+      <Suspense><SkillSearch :skills="() => $refs['dataSkills']" /></Suspense>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SkillSearch.vue
@@ -0,0 +1,90 @@
+<script setup>
+import {computed, ref} from 'vue'
+import SkillItem from "@/components/SkillItem.vue";
+import showdown from 'showdown'
+import sanitizeHtml from 'sanitize-html'
+
+const fileSkills = "./data/skills.json";
+const skills = ref(
+    await fetch(fileSkills)
+        .then(res => res.json())
+        .then(skillsJSON => {
+          // Convert and sanitise Markdown descriptions
+          // For unformatted description (for filtering), we want to
+          // replace all newlines with space since that's easier to search by
+          const converter = new showdown.Converter();
+          return skillsJSON.map(skillJSON => {
+            var finalDesc = "";
+            var unformattedDesc = "";
+            var paras = skillJSON.description.split("\n");
+            for (var para of paras) {
+              var desc = converter.makeHtml(para);
+              desc = sanitizeHtml(desc, {
+                allowedTags: [ 'b', 'strong', 'i', 'em', 'br' ],
+                allowedAttributes: {},
+                disallowedTagsMode: 'discard'
+              });
+              finalDesc += "<div>" + desc + "</div>";
+
+              unformattedDesc += " " + sanitizeHtml(desc.toLowerCase(), {
+                allowedTags: [],
+                allowedAttributes: {},
+                disallowedTagsMode: 'discard'
+              });
+            }
+
+            return {
+              "name": skillJSON.name,
+              "requirements": skillJSON.requirements,
+              "description": finalDesc,
+              "unformattedDescription": unformattedDesc,
+              "maxSkillLevel": skillJSON.maxSkillLevel
+            };
+          });
+        })
+);
+
+const filterName = ref("");
+const filterReq = ref("");
+const filterDesc = ref("");
+const filteredSkills = computed(() => {
+  if (filterName.value === "" && filterReq.value === "" && filterDesc.value === "")
+    return skills.value;
+  return skills.value.filter(skill => {
+    if (!skill.name.toLowerCase().includes(filterName.value.toLowerCase()))
+      return false;
+    if (!skill.unformattedDescription.toLowerCase().includes(filterDesc.value.toLowerCase()))
+      return false;
+    // TODO: Split requirements into CSV
+    if (!skill.requirements.toLowerCase().includes(filterReq.value.toLowerCase()))
+      return false;
+    return true;
+  });
+});
+
+</script>
+
+<template>
+    <header>
+      <h1>Skills</h1>
+    </header>
+
+    <main>
+      <div>
+        <h2>Filter</h2>
+        <strong>Name</strong>: <input type="text" v-model="filterName" /> <br />
+        <strong>Requirements</strong>: <input type="text" v-model="filterReq" /> <br />
+        <strong>Description</strong>: <input type="text" v-model="filterDesc" /> <br />
+      </div>
+      <ul v-if="skills">
+        <li v-for="skill in filteredSkills" v-bind:key="skill.name">
+          <SkillItem :skill=skill />
+        </li>
+      </ul>
+      <div v-else>Loading...</div>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
