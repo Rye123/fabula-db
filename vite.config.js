77cc401719d4a50692ee9da9620bf73201f6f714
Ryan Toh <ryantoh99@gmail.com>
2024-11-04 14:07:35 +0800

    fix: different envs for dev (local) and production (gh pages)

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/fabula-db/",
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, "./")
+
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+  }
 })
